9ef5b90ecc6f61f4a31b7c144bfdeedb659a2e3c
Stuart Pearman <swpearman@gmail.com>
2022-02-16 19:37:05 -0500

    Add eleventy-img image processing plugin

--- config/shortcodes.js
+++ config/shortcodes.js
@@ -1,10 +1,30 @@
 const path = require('path');
 const fs = require('fs');
 
+const Image = require('@11ty/eleventy-img');
 const Prism = require('prismjs');
 const loadLanguages = require('prismjs/components/');
 
 loadLanguages(['py', 'json']);
+
+async function generateImage (src, alt, sizes) {
+    let metadata = await Image(src, {
+      widths: [300, 600, 1100, 1600],
+      formats: ['avif', 'jpeg', 'png'],
+      outputDir: './public/images/',
+      urlPath: '/public/images/'
+    });
+  
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: 'lazy',
+      decoding: 'async',
+    };
+  
+    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPairedShortcode('highlight', function (code, language) {
@@ -23,4 +43,6 @@ module.exports = function (eleventyConfig) {
             .split(' ') : ''
         }">` + value + `</section>`;
     });
+
+    eleventyConfig.addNunjucksAsyncShortcode('image', generateImage);
 }
