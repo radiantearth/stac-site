648cf4849a72cadb36a31640db4fa5a26c284bef
PowerChell <michelle@radiant.earth>
2023-05-25 12:14:57 -0600

    Updating Tutorials landing page

--- content/tutorials.js
+++ content/tutorials.js
@@ -12,11 +12,17 @@ module.exports = {
         en: `
 # STAC Tutorials
 
-Here you can find our library of tutorials for learning all about STAC. You can find a list of even more tutorials from all over the web at the [stac index](https://stacindex.org/learn).
+Welcome to our library of tutorials for learning all about STAC. In addition to our own tutorials, you can also explore an
+extensive list of tutorials sourced from various corners of the web on the [STAC index](https://stacindex.org/learn).
 
-If there's a topic you'd like to learn more about, we recommend joining our [Gitter Chat](https://gitter.im/SpatioTemporal-Asset-Catalog/Lobby). There may well be a blog post on the topic published somewhere on the web that one of our community can direct you toward.
+If you're eager to delve deeper into a specific topic, we encourage you to join our 
+[Gitter Chat](https://gitter.im/SpatioTemporal-Asset-Catalog/Lobby) community. Chances are, one of our knowledgeable community members 
+can direct you to a relevant blog post or resource available online.
 
-We'll continue to add tutorials to the site bring new ways to learn and understand how to use STAC in the real world. If you'd like to contribute a tutorial of your own, we're acecepting pull requests to the [stac-site repository](https://github.com/radiantearth/stac-site).
+We are committed to regularly expanding our tutorial collection to provide you with fresh perspectives and practical insights 
+into leveraging STAC in real-world scenarios. We welcome contributions from users like you, so if you have an idea for a tutorial, 
+don't hesitate to submit a pull request to the [stac-site repository](https://github.com/radiantearth/stac-site) repository.
+
 `
     }
 }
